093cd18a86026835140e213313da0def6adcaeed
Jonathan Jin <jonathan@tentwentyfour.lu>
2016-07-19 14:35:40 +0200

    Added phpspec to require-dev for unit tests

--- import.php
+++ import.php
@@ -5,11 +5,13 @@
 namespace Remaim;
 
 use Redmine\Client;
+use Redmine\Api\Issue;
 use Symfony\Component\Yaml\Parser;
 use Symfony\Component\Yaml\Exception\ParseException;
 
 // There is a composer package for libphutil, but it's unofficial and not 100% compatible:
 // https://packagist.org/packages/mtrunkat/libphutil
+
 require_once '/usr/share/libphutil/src/__phutil_library_init__.php';
 require_once 'vendor/autoload.php';
 
@@ -18,10 +20,11 @@ require_once 'vendor/autoload.php';
     host:
     user:
     password:
-phabricator:
+    phabricator:
     host:
     token:
  */
+
 try {
     $yaml = new Parser();
     $config = $yaml->parse(file_get_contents('remaim.yml'));
@@ -44,7 +47,7 @@ function addToProject($container, $project)
         if ($key == $project['parent']['id']) {
             $slot[] = $project;
         } else {
-            //return addToProject($container, ...)	should have finished this …
+            //return addToProject($container, ...)  should have finished this …
         }
         return $slot;
     }, $container, array_keys($container));
@@ -68,7 +71,7 @@ $redmine = new Client(
 // since it never established a connection.
 $project_listing = $redmine->project->listing();
 if (empty($project_listing)) {
-	die("\n" . 'Your project list is empty or we were unable to connect to redmine. Check your credentials!' . "\n");
+    die("\n" . 'Your project list is empty or we were unable to connect to redmine. Check your credentials!' . "\n");
 }
 
 // First list available projects, then allow the user to select one
@@ -95,6 +98,65 @@ foreach ($projects as $project) {
 print('Select a project: [0]' . "\n");
 $fp = fopen('php://stdin', 'r');
 $project = trim(fgets($fp, 1024));
+
+/////
+$detail = $redmine->project->show($project);
+var_dump($detail); exit;
+
+if(!$detail || empty($detail)) {
+    printf('No projects found!');
+}
+
+
+
+$tasks = $redmine->issue->all([
+    'project_id' => $project,
+    'limit' => 1024
+]);    // 94 == VM Test Software
+if (!$tasks || empty($tasks['issues'])) {
+    printf('No tasks found on project %s', $project);
+}
+$issues = $tasks['issues'];
+
+$project_issuepriorities = $redmine->issuepriority->all([
+    'project_id' => $project,
+    'limit' => 1024
+]);
+var_dump($project_issuepriorities); 
+
+if($project_issuepriorities == Issue::PRIO_IMMEDIATE || $project_issuepriorities == Issue::PRIO_URGENT){
+    printf('Priority: Immediate/Unbreak now');
+}else{
+    if($project_issuepriorities == Issue::PRIO_HIGH){
+    printf('Priority: High');
+      }else{
+          if($project_issuepriorities == Issue::PRIO_NORMAL){
+          printf('Priority: Normal');
+          }else{
+              if($project_issuepriorities == Issue::PRIO_LOW)
+              {printf('Priority: Low');}
+           }
+
+     }
+}
+
+$relation = $redmine->issuerelation->all([
+    'project_id' => $project,
+    'limit' => 1024
+]);
+var_dump($relation);
+
+$project_issuerelation = $redmine->issuerelation->show($relation);
+var_dump($project_issuerelation); 
+exit;
+
+$project_issuestatus = $redmine->issuestatus->all([
+    'project_id' => $project,
+    'limit' => 1024
+]); 
+var_dump($project_issuestatus); 
+
+/////
 
 // Grab issues for the selected project
 $tasks = $redmine->issue->all([
@@ -129,9 +191,12 @@ $results = array_map(function ($issue) use ($conduit, $redmine) {
             ]
         ]
     );
-    var_dump($details); exit;
 
-    $description = str_replace("\r", '', $data[20]);
+    var_dump($details); 
+    $project_attachments = $redmine->attachment->show($attachment);
+    var_dump($project_attachments);
+
+    /*$description = str_replace("\r", '', $data[20]);
     $api_parameters = array(
         'title' => $data[6],
         'description' => $description,
@@ -145,7 +210,7 @@ $results = array_map(function ($issue) use ($conduit, $redmine) {
         'comments' => 'test comment',
     );
 
-    $result = $conduit->callMethodSynchronous('maniphest.update', $api_parameters);
+    $result = $conduit->callMethodSynchronous('maniphest.update', $api_parameters);*/
 }, $issues);
 
 // Make this nicer obviously ;)
